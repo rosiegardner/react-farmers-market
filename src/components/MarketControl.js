3214574028dc86af85896675dbc56fb4a7f99b09
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-04 16:06:28 -0700

    create file with component

---
+++ src/components/MarketControl.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SeasonalProduce from "./SeasonalProduce";
+
+function MarketControl(){
+  return (
+    <SeasonalProduce />
+  );
+}
+
+export default MarketControl;
