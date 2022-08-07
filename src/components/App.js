5e0d4d26c644996477172c29722a89433aba3dbe
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-07 14:45:33 -0700

    WIP - onward

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 function App(){
   const marketSchedule = "Sunday-Saturday";
   const availableProduce = "Varies by Season";
   return (
     <React.Fragment>
+      <Header />
       <h1>Rosie's Organics</h1>
       <h3>{marketSchedule}: Location and Produce {availableProduce}</h3>
       <p><em>Firebase entries not saving!</em></p>
